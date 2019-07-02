92459c9892743aa1d121670e7b267f7a7ff708a6
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2019-07-01 23:57:40 -0400

    Local component state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const list = [
@@ -20,22 +20,35 @@ const list = [
   },
 ];
 
-function App() {  
-  
-  return (
-    <div className="App">
-      {list.map(item =>         
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>         
-      )}   
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        {
+          this.state.list.map(item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 
 }
 
