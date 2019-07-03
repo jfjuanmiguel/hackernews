fbc6edda1626142216c2d9e3bff46979e09ce0e7
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2019-07-02 22:56:03 -0400

    Forms and events - High order functions

--- src/App.js
+++ src/App.js
@@ -20,22 +20,44 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      list: list,
+      list,
+      searchTerm: ''
     }
 
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+
+  }
+
+  onDismiss(id) {
+    const isNotId = (item) => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   render() {
     return (
-      <div className="App" >
+      <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
         {
-          this.state.list.map(item =>
+          this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -43,6 +65,13 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                >
+                  Dismiss
+                </button>
+              </span>
             </div>
           )
         }
