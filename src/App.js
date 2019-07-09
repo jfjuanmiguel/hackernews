d9ac567a5c82e62273fe153b3e5c3965842ec59f
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2019-07-09 18:37:17 -0400

    Controlled Components

--- src/App.js
+++ src/App.js
@@ -20,25 +20,24 @@ const list = [
   },
 ];
 
-const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       list,
-      searchTerm: ''
-    }
+      searchTerm: '',
+    };
 
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
-
   }
 
   onDismiss(id) {
-    const isNotId = (item) => item.objectID !== id;
+    const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
   }
@@ -48,37 +47,35 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, onSearchChange, list } = this.state;
+
     return (
-      <div className="App">
+      <div className='App'>
         <form>
           <input
-            type="text"
-            onChange={this.onSearchChange}
+            type='text'
+            value={searchTerm}
+            onChange={onSearchChange}
           />
         </form>
-        {
-          this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          )
-        }
+        {list.filter(isSearched(searchTerm)).map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)}>
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
       </div>
     );
   }
-
 }
 
 export default App;
